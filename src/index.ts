7f94d0d132a235a590cad3036cc5c927dd42d228
isaias3silva <isaias3silva@gmail.com>
2021-11-17 20:00:15 -0300

    Alter file index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { URLController } from 'controller/URLController'
 import express, { Request, Response } from 'express'
 
 const api = express()
+api.use(express.json())
 
 const urlcontroller = new URLController
 api.post("/shorten", urlcontroller.shorten)
